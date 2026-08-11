2290832f32b3fa904257d8913c50bc4d26af4b26
Preet <chandrakarpreet.1100@gmail.com>
2026-08-11 23:21:43 +0530

    Update StockTransactionService interface

--- bakery-inventory-backend/src/main/java/com/bakery/inventory/service/StockTransactionService.java
+++ bakery-inventory-backend/src/main/java/com/bakery/inventory/service/StockTransactionService.java
@@ -1,4 +1,14 @@
 package com.bakery.inventory.service;
 
+import com.bakery.inventory.dto.stocktransaction.StockTransactionResponse;
+import java.util.List;
+
 public interface StockTransactionService {
+    StockTransactionResponse getTransactionById(Integer id);
+
+    List<StockTransactionResponse> getAllTransactions();
+
+    List<StockTransactionResponse> getTransactionsByProductId(Integer productId);
+
+    List<StockTransactionResponse> getTransactionsByInventoryId(Integer inventoryId);
 }
